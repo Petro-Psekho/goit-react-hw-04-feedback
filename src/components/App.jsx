d7ed2defd210afa65e061f15667a39c9f4ad663f
Petro Psekho <psekho.pp@gmail.com>
2023-02-28 23:15:33 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { useState } from 'react';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
